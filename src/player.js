5d445d4e9593e301c24791a35918cf86c73adaeb
bodek98 <bodzio.entire@gmail.com>
2022-12-14 21:49:31 +0100

    Explosion of the bomb

--- src/player.js
+++ src/player.js
@@ -17,33 +17,65 @@ class Player {
   placeBomb(map) {
     map[this.position.x][this.position.y] = new Tile("bomb");
     // Add bomb handling
-    // explodeBomb(this.position.x, this.position.y);
+    explodeBomb(map, this.position.x, this.position.y);
   }
 }
 
-function explodeBomb(map) {
-  let mapFireList = [
-    { x: currentX, y: currentY },
-    { x: currentX + 1, y: currentY },
-    { x: currentX + 2, y: currentY },
-    { x: currentX + 3, y: currentY },
-    { x: currentX, y: currentY - 1 },
-    { x: currentX, y: currentY - 2 },
-    { x: currentX, y: currentY - 3 },
-  ];
-  mapFireList.forEach((element) => {
-    map[element.x][element.y] = new Tile("fire");
-  });
-  // dodać kolizje
-  isBombPlaced = false;
-  setTimeout(() => {
-    expireFire();
-  }, 1500);
+function isOutOfBounds(x, y, mapSize) {
+  const isX = x < 0 || x >= mapSize;
+  const isY = y < 0 || y >= mapSize;
 
-  function expireFire() {
-    mapFireList.forEach((element) => {
-      map[element.x][element.y] = new Tile("path");
-    });
-    // po dodaniu kolizji będzie OK
+  return isX || isY;
+}
+
+function explodeBomb(map, x, y) {
+  const mapSize = 13;
+  console.log("bum");
+  const fireLenght = 3;
+
+  function createFireTile(x, y) {
+    console.log("kafelek");
+    let parent = document.querySelector("#tile-" + x + "-" + y);
+    let fireTile = document.createElement("div");
+    fireTile.classList.add("tile-fire");
+    parent.appendChild(fireTile);
+  }
+
+  function expandFire(nextX, nextY) {
+    if (!isOutOfBounds(nextX, nextY, mapSize)) {
+      if (map[nextX][nextY].tileType === "wall") {
+        return false;
+      } else {
+        createFireTile(nextX, nextY);
+      }
+    }
+    return true;
+  }
+
+  // Center
+  createFireTile(x, y);
+  // Right
+  for (let i = 1; i <= fireLenght; i++) {
+    if (!expandFire(x + i, y)) {
+      break;
+    }
+  }
+  // Left
+  for (let i = 1; i <= fireLenght; i++) {
+    if (!expandFire(x - i, y)) {
+      break;
+    }
+  }
+  // Up
+  for (let i = 1; i <= fireLenght; i++) {
+    if (!expandFire(x, y + i)) {
+      break;
+    }
+  }
+  // Down
+  for (let i = 1; i <= fireLenght; i++) {
+    if (!expandFire(x, y - i)) {
+      break;
+    }
   }
 }
